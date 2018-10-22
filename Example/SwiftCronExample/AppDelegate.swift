efa1b80a3d7048d9e168ec4a80197eccfd7b08b8
Keegan Rush <keeganrush@gmail.com>
2018-10-22 04:41:40 +0200

    Bump Swift version to 4.2

--- Example/SwiftCronExample/AppDelegate.swift
+++ Example/SwiftCronExample/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(
-    _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
+        _ application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
     -> Bool {
 		return true
 	}
